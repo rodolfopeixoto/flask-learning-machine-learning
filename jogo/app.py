18b34bdf4ee7c3de770125d7d4da137bdb801717
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2018-03-20 00:22:19 -0300

    Add folder templates and html

--- jogo/app.py
+++ jogo/app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Flask Dockerized'
+    return render_template('lista.html')
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
